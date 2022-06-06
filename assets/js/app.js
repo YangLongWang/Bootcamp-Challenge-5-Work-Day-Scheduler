dd8f05f345d96b38ee4157a20179efc4be7cb8b8
yang <joey674901205@gmail.com>
2022-06-06 14:27:04 -0400

    color block over, merge into develop

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,27 +2,27 @@ var dayNtime = $("#currentDay").text(dayjs().format('MMM D dddd, h:mmA'));
 var saveBtn = $(".saveBtn").on("click", saveContent);
 var colorBlock = $("textarea");
 
+// toNumber 24hours('H')
+var nowHour = parseInt(dayjs().format('H')); 
 
 $(".time-block").each(function() {
-    var nowHour = parseInt(dayjs().format('H')); // toNumber 24hours('H')
-    var timeBlock = parseInt($(this).attr("id")); // toNumber time-block
-    if (timeBlock > nowHour) { // future show green
-        colorBlock.addClass("future");
-        colorBlock.removeClass("past");
-        colorBlock.removeClass("present");
-    } else if (timeBlock < nowHour) { // past show gray
-        colorBlock.addClass("past");
-        colorBlock.removeClass("future");
-        colorBlock.removeClass("present");
-    } else { // present show red
-        colorBlock.addClass("present");
-        colorBlock.removeClass("future");
-        colorBlock.removeClass("past");
+    // toNumber time-block
+    var timeBlock = parseInt($(this).attr("id")); 
+    if (timeBlock > nowHour) { 
+        // future show green
+        $(this).toggleClass("future");
+
+    } else if (timeBlock < nowHour) { 
+        // past show gray
+        $(this).toggleClass("past");
+
+    } else { 
+        // present show red
+        $(this).toggleClass("present");
+
     }
 });
-// colorBlock.toggleClass("past");
-// colorBlock.toggleClass("present");
-// colorBlock.toggleClass("future");
+
 
 
 // event.preventDefault()
