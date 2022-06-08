543af2e664436ae80ff6363e690e2e4374a1a896
yang <joey674901205@gmail.com>
2022-06-08 02:17:42 -0400

    first step, can enter text in textarea, and then when click save button, that's text can save into localstorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
+// day.js display date
 var dayNtime = $("#currentDay").text(dayjs().format('MMM D dddd, h:mmA'));
-var saveBtn = $(".saveBtn").on("click", saveContent);
-var colorBlock = $("textarea");
+var input = $("textarea");
+var tasks = [];
 
-// toNumber 24hours('H')
-var nowHour = parseInt(dayjs().format('H')); 
 
+// according to time show different color block
 $(".time-block").each(function() {
+    // toNumber 24hours('H')
+    var nowHour = parseInt(dayjs().format('H'));     
     // toNumber time-block
     var timeBlock = parseInt($(this).attr("id")); 
     if (timeBlock > nowHour) { 
@@ -23,14 +25,48 @@ $(".time-block").each(function() {
     }
 });
 
+// create content
+$("textarea").each(function() {
 
+    // var noteInput = $("textarea");
+    // noteInput.text = noteInput;
+    // console.log(noteInput.text);
 
-// event.preventDefault()
+    // tasks.push(noteInput);
+    // console.log(tasks);
 
+    // $(this).on("click", "textarea", function() {
+        // var isEdit = $(this).hasAttribute();
 
+        // var noteInput = $("textarea").text();
+        // tasks.push(noteInput);
+        // console.log(tasks);
 
+    // })
 
+    // var note = $(this).text().trim();
+    // var noteInput = $("textarea").addClass("description col-10").val(note);
+    // $(this).replaceWith(noteInput);
+})
+
+// event.preventDefault();
+
+// after clicking the button, the info can save into localstorage 
 function saveContent() {
-    console.log("123");
+    tasks.push(input.val());
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 
 }
+// JSON.stringify(tasks)
+
+function loadContent() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+
+var saveBtn = $(".saveBtn").on("click", saveContent);
+// var contentEl = $(".time-block").on("click", "textarea", enterContent);
+
+
+
+loadContent();
